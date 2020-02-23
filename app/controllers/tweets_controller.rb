bceb2358511862fc44099ce4b5923e1fd950c50f
ayaszmr <s.aya@live.com>
2020-02-23 21:34:33 +0900

    編集機能追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -19,6 +19,10 @@ class TweetsController < ApplicationController
     tweet.destroy if tweet.user_id == current_user.id
   end
 
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
   private
   def tweet_params
     params.permit(:image, :text)
