208d25593054d2c4ef61b92789d4c0446b105990
ayaszmr <s.aya@live.com>
2020-02-23 20:31:09 +0900

    name カラム 削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_22_123151) do
+ActiveRecord::Schema.define(version: 2020_02_23_112745) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
     t.text "text"
     t.text "image"
     t.datetime "created_at"
