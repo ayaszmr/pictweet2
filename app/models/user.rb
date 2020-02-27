e5cd5314963c6efe8893448535dba0f943ab8365
ayaszmr <s.aya@live.com>
2020-02-27 19:23:43 +0900

    いいね view 追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :liked_tweets, through: :likes, source: :tweet
 
-  def already_liked(tweet)
+  def already_liked?(tweet)
     self.likes.exists?(tweet_id: tweet.id)
   end
 end
