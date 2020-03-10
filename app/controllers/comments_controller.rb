7b759f1f975edf1647e84ccdaccaef69d2186fc5
ayaszmr <s.aya@live.com>
2020-03-10 19:05:13 +0900

    非同期うまくゆかず。。

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,12 @@
-class CommentsController < ApplicationController
-  def create
-    Comment.create(text: comment_params[:text], tweet_id: comment_params[:tweet_id], user_id: current_user.id)
+def create
+  @comment = Comment.create(comment_params)
+  respond_to do |format|
+    format.html { redirect_to tweet_path(params[:tweet_id]) }
+    format.json
   end
+end
 
-  private
-  def comment_params
-    params.permit(:text, :tweet_id)
-  end
+private
+def comment_params
+  params.require(:comment).permit(:text).merge(user_id: current_user.id, tweet_id: params[:tweet_id])
 end
